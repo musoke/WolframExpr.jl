00a0f41ff138c7610eb390112c3a8be4cb272bd7
Documenter.jl <documenter@juliadocs.github.io>
2023-07-06 20:30:32 +0000

    build based on 4aa0986

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WolframExpr","category":"page"},{"location":"#WolframExpr","page":"Home","title":"WolframExpr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WolframExpr.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WolframExpr]","category":"page"}]
+}
