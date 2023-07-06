849fa49effb14d19ddc542bccc02b0f05ef8110b
Nathan Musoke <nathan.musoke@gmail.com>
2023-07-06 16:45:12 -0400

    ci: Add JuliaFormatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,20 @@
 using WolframExpr
 using Documenter
 
-DocMeta.setdocmeta!(WolframExpr, :DocTestSetup, :(using WolframExpr); recursive=true)
+DocMeta.setdocmeta!(WolframExpr, :DocTestSetup, :(using WolframExpr); recursive = true)
 
 makedocs(;
-    modules=[WolframExpr],
-    authors="Nathan Musoke <nathan.musoke@gmail.com> and contributors",
-    repo="https://github.com/musoke/WolframExpr.jl/blob/{commit}{path}#{line}",
-    sitename="WolframExpr.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://musoke.github.io/WolframExpr.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [WolframExpr],
+    authors = "Nathan Musoke <nathan.musoke@gmail.com> and contributors",
+    repo = "https://github.com/musoke/WolframExpr.jl/blob/{commit}{path}#{line}",
+    sitename = "WolframExpr.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://musoke.github.io/WolframExpr.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/musoke/WolframExpr.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/musoke/WolframExpr.jl", devbranch = "main")
