30e0a5b7f86693b535df43567df75ff26bc60c98
Nathan Musoke <nathan.musoke@gmail.com>
2023-07-11 18:43:25 -0400

    Demo of hanging doctests with debug output from Documenter

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,15 @@ using WolframExpr
 using Test
 using Documenter
 
+ENV["JULIA_DEBUG"]="Documenter"
 doctest(WolframExpr)
 
 @testset "WolframExpr.jl" begin
     include("basic.jl")
 end
 
-@testset "Aqua.jl" begin
-    using Aqua
+# @testset "Aqua.jl" begin
+#     using Aqua
 
-    Aqua.test_all(WolframExpr)
-end
+#     Aqua.test_all(WolframExpr)
+# end
